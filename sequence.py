3a780c2284162412d41c51b669575eaa56ce401b
matt venn <matt@mattvenn.net>
2019-04-26 21:01:39 +0200

    fixed lost sequences

--- sequence.py
+++ sequence.py
@@ -7,9 +7,84 @@ class ShotType(Enum):
 
 sequence = [
     {
+        "time" : (0,15),
+        "type" : ShotType.title,
+        "text" : "intro to cover",
+    },
+    {
+        "time" : (0,40),
+        "type" : ShotType.title,
+        "text" : "testbenches",
+    },
+    {
+        "time" : (1,47),
+        "type" : ShotType.title,
+        "text" : "makefile",
+    },
+    {
+        "time" : (2,9),
+        "type" : ShotType.title,
+        "text" : "gtkwave",
+    },
+    {
+        "time" : (2,40),
+        "type" : ShotType.title,
+        "text" : "4 LEDs parameter",
+    },
+    {
+        "time" : (3,10),
+        "type" : ShotType.title,
+        "text" : "alternative to testbench: cover",
+    },
+    {
+        "time" : (3,21),
+        "type" : ShotType.speedup,
+        "speed": 5,
+    },
+    {
+        "time" : (4,40),
+        "type" : ShotType.title,
+        "text" : "the cover statement",
+    },
+    {
+        "time" : (4,50),
+        "type" : ShotType.title,
+        "text" : "sby config",
+    },
+    {
+        "time" : (5,0),
+        "type" : ShotType.title,
+        "text" : "makefile",
+    },
+    {
+        "time" : (5,22),
+        "type" : ShotType.title,
+        "text" : "run cover with sby",
+    },
+    {
+        "time" : (5,30),
+        "type" : ShotType.title,
+        "text" : "cover fails",
+    },
+    {
+        "time" : (6,0),
+        "type" : ShotType.title,
+        "text" : "ws2812 long reset time",
+    },
+    {
+        "time" : (6,30),
+        "type" : ShotType.title,
+        "text" : "change params",
+    },
+    {
+        "time" : (6,47),
+        "type" : ShotType.speedup,
+        "speed" : 5,
+    },
+    {
         "time" : (7,05),
         "type" : ShotType.title,
-        "text" : "ws2812 parameters",
+        "text" : "ws2812 timing",
     },
     {
         "time" : (7,32),
@@ -75,3 +150,25 @@ sequence = [
         "type" : ShotType.end,
     },
 ]
+"""
+4:50,sby config
+5:00,makefile
+5:22,run cover with sby
+5:30,cover fails
+6:00,ws2812 long reset time
+6:30,change params
+6:47,SPEEDUP
+7:05,ws2812 timing
+7:32,CUT
+8:01,webpage...
+8:50,cover reached
+9:06,SPEEDUP
+9:30,makefile ORing
+10:00,append
+11:15,cover led data
+11:50,write signal is now toggling
+12:26,2 LEDs
+12:45,AABBCC data is seen
+12:50,outro
+13:10,COMMENTS!!!
+"""
