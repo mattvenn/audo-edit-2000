9d9d9766c93b2491f355b06a5cb13b9843f8fe3f
matt venn <matt@dygma.com>
2019-05-10 20:58:23 +0200

    version4

--- docker/setup.sh
+++ docker/setup.sh
@@ -4,5 +4,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 apt update
 apt -y install docker-ce
-docker pull mattvenn/moviepy:install_3
-
+docker pull mattvenn/moviepy:install_4
