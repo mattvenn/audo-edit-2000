6d51cfdfc4239807e8e13621c3785bded8211184
matt venn <matt@mattvenn.net>
2019-05-28 20:48:26 +0200

    start in bg so can run both at once

--- record-tools/record.screen.sh
+++ record-tools/record.screen.sh
@@ -1,3 +1,3 @@
 echo $1
 #recordmydesktop --on-the-fly-encoding --no-sound --fps 20 --width 2560 --height 1700 -x0 -y0 --v_quality=20 -o $1.ogv
-ffmpeg -nostdin -video_size 2560x1600 -framerate 20 -f x11grab -i :0.0+0,0 -vf scale=1280:800 $1.mkv 
+ffmpeg -nostdin -video_size 2560x1600 -framerate 20 -f x11grab -i :0.0+0,0 -vf scale=1280:800 $1.mkv > $1.log 2>&1
--- record-tools/record.webcam.sh
+++ record-tools/record.webcam.sh
@@ -1,4 +1,4 @@
 echo $1
 #ffmpeg -f alsa -i hw:3 -f v4l2 -framerate 20 -video_size 800x600 -input_format mjpeg -i /dev/video0 -preset faster $1.mkv
 ffmpeg -nostdin -f alsa -thread_queue_size 2048 -sample_rate 44100 -i hw:3 \
--f v4l2 -thread_queue_size 2048 -framerate 20 -video_size 800x600 -input_format mjpeg -i /dev/video0  $1.mkv
+-f v4l2 -thread_queue_size 2048 -framerate 20 -video_size 800x600 -input_format mjpeg -i /dev/video0  $1.mkv > $1.log 2>&1
